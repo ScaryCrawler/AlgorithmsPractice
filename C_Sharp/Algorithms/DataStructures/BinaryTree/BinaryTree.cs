d5b5ac05f48537247b22ea6424eeb8af9dea24de
Ilay <sakhno.iv@gmail.com>
2018-01-28 16:57:59 +0300

    Project was reorganized

---
+++ C_Sharp/Algorithms/DataStructures/BinaryTree/BinaryTree.cs
@@ -0,0 +1,25 @@
+﻿namespace DataStructures.BinaryTree
+{
+    public class BinaryTree
+    {
+        public class Tree {
+            public static TreeNode InvertTree(TreeNode root)
+            {
+                var tmp = root.Right;
+                root.Right = root.Left;
+                root.Left = root.Right;
+
+                InvertTree(root.Left);
+                InvertTree(root.Right);
+                return root;
+            }
+        }
+
+        public class TreeNode
+        {
+            public int Value { get; set; }
+            public TreeNode Left { get; set; }
+            public TreeNode Right { get; set; }
+        }
+    }
+}
--- C_Sharp/Algorithms/LinkedList/Program.cs
+++ C_Sharp/Algorithms/LinkedList/Program.cs
@@ -1,15 +1,12 @@
 ﻿namespace LinkedList
 {
+
+    
     internal class Program
     {
         public static void Main(string[] args)
         {
-            LinkedList<int> testList = new LinkedList<int>();
-            testList.AddNode(123);
-            testList.AddNode(1111);
-            testList.AddNode(777);
             
-            testList.PrintList();
         }
     }
 }
